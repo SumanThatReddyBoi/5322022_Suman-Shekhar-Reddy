87083a4423e624ed363e5dba213fe114b5e77209
Suman Reddy <122171541+SumanThatReddyBoi@users.noreply.github.com>
2025-08-05 14:21:18 +0530

    Add files via upload

---
+++ HackerRank/Week 1/lonelyinteger.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    } for(int i=0; i<n; i++) {
+        int count = 0;
+    for(int j=0; j<n; j++) {
+        if(arr[i]==arr[j]) {
+            count=count+1;
+        }
+    }
+        if(count == 1) {
+            printf("%d ", arr[i]);
+        }
+    }
+}
